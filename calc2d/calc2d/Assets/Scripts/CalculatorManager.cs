1cf6336b60c58711a6aa32c6173310a33579f082
marabesi <matheus.marabesi@gmail.com>
2016-09-04 21:40:47 -0300

    calc2d added

---
+++ calc2d/calc2d/Assets/Scripts/CalculatorManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CalculatorManager : MonoBehaviour {
+
+	public Text result;
+	public Button btn1;
+	public Button btn2;
+	public Button btn3;
+	public Button btn4;
+	public Button btn5;
+	public Button btn6;
+	public Button btn7;
+	public Button btn8;
+	public Button btn9;
+	public Button btn0;
+
+	void Start()
+	{
+		result = GameObject.Find("result").GetComponent<Text>();
+	}
+
+	void addNumber(Text number)
+	{
+		string total = result.text;
+
+		if (total == "0") {
+			total = "";
+		}
+
+		total += number.text;
+
+		result.text = total;
+	}
+	
+	void cleanNumbers()
+	{
+		result.text = "0";
+	}
+}
